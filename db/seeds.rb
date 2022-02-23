8b560a99c6e624c48dfcac7cf71ec99592436e0d
Leonardo Reigota <leoreigotinha@gmail.com>
2022-02-23 16:41:11 -0300

    solved challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: '17996125536', category: 'chinese' }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: '17996125536', category: 'italian' }
+bullguer = { name: "bullguer", address: "wisard", phone_number: '17996125536', category: 'french' }
+vintao = { name: "vintao", address: "fidalga", phone_number: '17996125536', category: 'japanese' }
+tribunal = { name: "tribunal", address: "jerico", phone_number: '17996125536', category: 'belgian' }
+
+[ dishoom, pizza_east, bullguer, vintao, tribunal].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
